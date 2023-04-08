bfc36c649345a0cab095ccebb19dbe00a009382a
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:57:05 -0400

    Delete ChatGPT Python directory

--- ChatGPT Python/Easy_242_Code_Python3.py
+++
@@ -1,4 +0,0 @@
-# sort both strings and compare if they are equal
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
--- ChatGPT Python/Easy_492_Code_Python3.py
+++
@@ -1,8 +0,0 @@
-
-class Solution:
-    def constructRectangle(self, area: int) -> List[int]:
-        import math
-        sqrt_area = int(math.sqrt(area)) # square root of area. Convert to an integer.
-        while area % sqrt_area != 0: # keep decrementing until it is divisible by the sqrt_area
-            sqrt_area -= 1
-        return [area // sqrt_area, sqrt_area] # return the length and width
